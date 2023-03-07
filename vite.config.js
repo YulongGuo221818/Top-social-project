4d2ebb5e97eccd28f1e731e45f68e0645bf77765
leo.liu <leo.liu@unloan.com.au>
2023-03-08 08:10:09 +1100

    add prettierrc

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   root: './src',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-})
+});
